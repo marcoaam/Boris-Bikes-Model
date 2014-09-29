2ff5cc9fdcd524f69a2d52aa455359278a8ad1b8
marcoaam <marco.army@gmail.com>
2014-09-29 22:21:09 +0100

    initial commit bike class

---
+++ Bike.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BorisBikes
+{
+	public class Bike
+	{
+		private bool Broken = false;
+
+		public bool IsBroken()
+		{
+			return Broken;
+		}
+
+		public void Break()
+		{
+			Broken = true;
+		}
+	}
+}
+
---
+++ NUnitTestBike.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace BorisBikes
+{
+	[TestFixture ()]
+	public class NUnitTestBike
+	{
+		[Test ()]
+		public void IsBikeBroken ()
+		{
+			Bike bike = new Bike ();
+			Assert.IsFalse(bike.IsBroken());
+		}
+
+		[Test ()]
+		public void BreakBike ()
+		{
+			Bike bike = new Bike ();
+			bike.Break();
+			Assert.IsTrue(bike.IsBroken());
+		}
+	}
+}
+
