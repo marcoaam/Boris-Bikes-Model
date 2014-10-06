9707a817f98ae71335363f223a14a6297ebf34b0
marcoaam <marco.army@gmail.com>
2014-10-06 10:37:57 +0100

    update

--- Lib/Program.cs
+++ Lib/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 
 namespace BorisBikes
 {
@@ -6,14 +7,24 @@ namespace BorisBikes
 	{
 		public static void Main (string[] args)
 		{
-//			Person marco = new Person ();
-//			Station station = new Station ();
-//
-//			Console.WriteLine (marco.HasBike());
-//			marco.RentBikeFrom (station);
-//			Console.WriteLine (marco.HasBike ());
-//			Console.WriteLine (marco.Bike);
-//			Console.ReadLine ();
+			Station station = new Station ("Old Street");
+			ArrayList bikes = new ArrayList ();
+			int x = 0;
+			for (x = 1 ; x < 20; x++) {
+				bikes.Add (new Bike ());
+			}
+
+			foreach (Bike bike in bikes) { bike.Break; }
+			station.Add (bikes);
+			Console.WriteLine ();
+			Person marco = new Person ();
+			Console.WriteLine (station.Bikes.Count);
+
+			Console.WriteLine (marco.HasBike());
+			marco.RentBikeFrom (station);
+			Console.WriteLine (marco.HasBike ());
+			Console.WriteLine (marco.Bike);
+			Console.ReadLine ();
 		}
 	}
 }
